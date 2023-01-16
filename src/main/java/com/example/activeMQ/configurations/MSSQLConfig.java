1cf1fb57cc069032240ba30ff84f4b52eec89b35
PhucPham96 <phucpham0396@gmail.com>
2023-01-16 08:44:16 +0700

    config bean sqlcomponent

--- src/main/java/com/example/activeMQ/configurations/MSSQLConfig.java
+++ src/main/java/com/example/activeMQ/configurations/MSSQLConfig.java
@@ -1,9 +1,23 @@
 package com.example.activeMQ.configurations;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.camel.component.sql.SqlComponent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-@ImportResource({"classpath:beans/beans-mssql.xml"})
+import javax.sql.DataSource;
+
 @Configuration
+@RequiredArgsConstructor
 public class MSSQLConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public SqlComponent sql() {
+        SqlComponent sqlComponent = new SqlComponent();
+        sqlComponent.setDataSource(dataSource);
+        return sqlComponent;
+    }
 }
